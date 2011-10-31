8c227bbdbc90d909bcf3152e09f5e3c591a6d765
Moritz Venn <moritz.venn@freaque.net>
2011-10-31 15:22:59 +0100

    CXMLNode.m: make sure we don't add nil-element to arrays
    Theoretically the "created" elements could be nil which in turn would
    result in crash.
    If there is an actual underlying problem this just hides it, but this is
    much better than mysterious crashes in release builds.

--- Source/CXMLNode.m
+++ Source/CXMLNode.m
@@ -180,7 +180,8 @@ else
 		while (theCurrentNode != NULL)
 		{
 			CXMLNode *theNode = [CXMLNode nodeWithLibXMLNode:theCurrentNode freeOnDealloc:NO];
-			[theChildren addObject:theNode];
+			if(theNode)
+				[theChildren addObject:theNode];
 			theCurrentNode = theCurrentNode->next;
 		}
 	}
@@ -347,7 +348,9 @@ else
 	for (N = 0; N < theXPathObject->nodesetval->nodeNr; N++)
 		{
 		xmlNodePtr theNode = theXPathObject->nodesetval->nodeTab[N];
-		[theArray addObject:[CXMLNode nodeWithLibXMLNode:theNode freeOnDealloc:NO]];
+		CXMLNode *theNode = [CXMLNode nodeWithLibXMLNode:theCurrentNode freeOnDealloc:NO];
+		if(theNode)
+			[theArray addObject:theNode];
 		}
 		
 	theResult = theArray;
